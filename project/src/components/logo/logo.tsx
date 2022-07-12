dff3a4261889dc79546b48a275f98f0d5c52132c
Sergey <szaytsev060576@gmail.com>
2022-07-12 22:45:01 +0300

    дорабатывает Logo

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -1,11 +1,18 @@
-function Logo(): JSX.Element {
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+type LogoProps = {
+  className?: string;
+}
+
+function Logo({ className }: LogoProps): JSX.Element {
   return (
     <div className="logo">
-      <a href="main.html" className="logo__link">
+      <Link to={AppRoute.Main} className={`logo__link ${className}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
-      </a>
+      </Link>
     </div>
   );
 }
