de7a9cbddccd490abb3d3009610b0d2e46ea3593
Sergey <szaytsev060576@gmail.com>
2022-08-26 22:27:02 +0300

    исправляет ошибку

--- project/src/store/film-screening/selector.ts
+++ project/src/store/film-screening/selector.ts
@@ -2,7 +2,7 @@ import { NameSpace } from '../../const';
 import { Films } from '../../types/films';
 import { State } from '../../types/state';
 
-export const getFilms = (state: State): Films => state[NameSpace.FilmScreening].rawFilms;
+export const getFilms = (state: State): Films => state[NameSpace.FilmScreening].films;
 export const getLoadedDataStatus = (state: State): boolean => state[NameSpace.FilmScreening].isDataLoaded;
 
 export const getFilteredFilms = (state: State): Films => state[NameSpace.FilmScreening].filteredFilms;
