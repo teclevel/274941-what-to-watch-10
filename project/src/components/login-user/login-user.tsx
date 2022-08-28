b6d359a34b63b4339f53c544215b94a878aaa90d
Sergey <szaytsev060576@gmail.com>
2022-08-28 21:03:12 +0300

    отправляет на mylist по аватарке

--- project/src/components/login-user/login-user.tsx
+++ project/src/components/login-user/login-user.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { logoutAction } from '../../store/api-actions';
@@ -8,12 +8,16 @@ import { isCheckedLogin } from '../../utils';
 function LoginUser(): JSX.Element {
   const dispatch = useAppDispatch();
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  const navigate = useNavigate();
+  const clickAvatarHandle = () => navigate(AppRoute.MyList);
 
   return (isCheckedLogin(authorizationStatus)
     ?
     <ul className="user-block">
       <li className="user-block__item">
-        <div className="user-block__avatar">
+        <div className="user-block__avatar"
+          onClick={clickAvatarHandle}
+        >
           <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
         </div>
       </li>
