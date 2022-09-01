97a4cf4eb2e4debd9aee06237547f65743cb8d17
Sergey <szaytsev060576@gmail.com>
2022-09-01 17:36:06 +0300

    подключает преход  по карточке

--- project/src/components/films-card/films-card.tsx
+++ project/src/components/films-card/films-card.tsx
@@ -1,5 +1,5 @@
 import { memo, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Film } from '../../types/films';
 import VideoPlayer from '../video-player/video-player';
 
@@ -8,11 +8,11 @@ type FilmsCardProps = {
 }
 
 function FilmsCard({ film }: FilmsCardProps,): JSX.Element {
+  const navigate = useNavigate();
   const { previewImage, name, previewVideoLink, id } = film;
 
   const [isPlayerOn, setIsPlayerOn] = useState(false);
   const [timerId, setTimerId] = useState<number>();
-
 
   const mouseOverHandle = () => {
     setTimerId(window.setTimeout(() => setIsPlayerOn(true), 1000));
@@ -27,6 +27,8 @@ function FilmsCard({ film }: FilmsCardProps,): JSX.Element {
     <article className="small-film-card catalog__films-card"
       onMouseOver={mouseOverHandle}
       onMouseOut={mouseOutHandle}
+      onClick={() => navigate(`/films/${id}`)}
+      style={{cursor:'pointer'}}
     >
       <div className="small-film-card__image">
         {
@@ -44,7 +46,7 @@ function FilmsCard({ film }: FilmsCardProps,): JSX.Element {
         }
       </div>
       <h3 className="small-film-card__title">
-        <Link className="small-film-card__link" to={`/films/${id}`}>
+        <Link className="small-film-card__link" to='/'>
           {name}
         </Link>
       </h3>
