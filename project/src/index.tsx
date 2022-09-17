982c4ad97b253ede0f7c915b207074f6bd001704
Sergey <szaytsev060576@gmail.com>
2022-09-17 21:05:20 +0300

    del hashrouter

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,7 +6,6 @@ import { store } from './store';
 import { checkAuthAction, fetchLoadFilmsAction, fetchLoadPromoAction } from './store/api-actions';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { HashRouter } from 'react-router-dom';
 
 store.dispatch(fetchLoadFilmsAction());
 store.dispatch(fetchLoadPromoAction());
@@ -18,12 +17,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
-
-      <Provider store={store}>
-        <ToastContainer />
-        <App />
-      </Provider>
-    </HashRouter>
+    <Provider store={store}>
+      <ToastContainer />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
